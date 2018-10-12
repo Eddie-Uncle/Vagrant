51562984ac51d3c621eaee83a6ec1fa16dafd8ce
ebsantos <ebsantos@snake02.localdomain>
2018-10-12 18:37:40 -0300

    FIX

---
+++ ubuntu/stopVMs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Stopping vms"
+vagrant halt snake05 && vagrant halt snake06 && vagrant snake07
+if [ "$?" -eq 0 ]
+then
+    echo "VMs have been shut down normally"
+else
+    echo "VMs have not been shut down  normally -- please check"
+fi
